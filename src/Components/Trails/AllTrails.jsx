2fb28fcae17d3ad5d66a6924cb4e0fedd3d62c21
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-03-18 14:45:42 -0400

    trail filters refactored!

--- src/Components/Trails/AllTrails.jsx
+++ src/Components/Trails/AllTrails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Button, ButtonGroup, Table } from 'reactstrap';
+import { Input, Button, Table, Form, FormGroup, Label } from 'reactstrap';
 import APIURL from '../../helpers/environment';
 
 
@@ -8,8 +8,10 @@ const AllTrails = (props) => {
     const [allTrails, setAllTrails] = useState([]);
     const [displayedTrails, setDisplayedTrails] = useState([]);
 
-    const [cSelected, setCSelected] = useState([]);
-    const [stateFilter, setStateFilter] = useState('');
+    const [stateFiltering, setStateFiltering] = useState(false);
+    const [stateFilter, setStateFilter] = useState('Alabama');
+
+    const [difficultyFiltering, setDifficultyFiltering] = useState(false);
     const [difficultyFilter, setDifficultyFilter] = useState('easy');
 
 
@@ -55,101 +57,118 @@ const AllTrails = (props) => {
         }
     }
 
-    // this is reactstrap code that allows you to select multiple filters
-    const onCheckboxBtnClick = (selected) => {
-        const index = cSelected.indexOf(selected);
-        if (index < 0) {
-            cSelected.push(selected);
-        } else {
-            cSelected.splice(index, 1);
-        }
-        setCSelected([...cSelected]);
-    }
+    const clearFilters = () => {
+        document.getElementById('stateCheckbox').checked = false;
+        setStateFiltering(false);
+        setStateFilter('Alabama');
 
-    const filterTrailsByState = (state) => {
-        let filteredTrails = displayedTrails.filter(trail => trail.location.state === state);
-        setDisplayedTrails(filteredTrails);
-    }
+        document.getElementById('difficultyCheckbox').checked = false;
+        setDifficultyFiltering(false);
+        setDifficultyFilter('easy');
 
-    const filterTrailsByDifficulty = (difficulty) => {
-        let filteredTrails = displayedTrails.filter(trail => trail.difficulty === difficulty);
-        setDisplayedTrails(filteredTrails);
-    }
-
-    const clearFilter = () => {
         setDisplayedTrails(allTrails);
-        setCSelected([]);
+    }
+
+    const filteredSearch = (e) => {
+        e.preventDefault();
+
+        let filteredTrails;
+
+        if (stateFiltering && difficultyFiltering) {
+            filteredTrails = displayedTrails.filter(trail => trail.location.state === stateFilter && trail.difficulty === difficultyFilter);
+        } else if (stateFiltering) {
+            filteredTrails = displayedTrails.filter(trail => trail.location.state === stateFilter);
+        } else if (difficultyFiltering) {
+            filteredTrails = displayedTrails.filter(trail => trail.difficulty === difficultyFilter);
+        } else {
+            console.log('no filters specified');
+            return;
+        }
+
+        setDisplayedTrails(filteredTrails);
     }
 
     return (
         <div>
             <h5>what kind of trails are you looking for, partner?</h5>
-            <ButtonGroup>
-                <Input type='select' value={stateFilter} onChange={e => setStateFilter(e.target.value)}>
-                    <option>State</option>
-                    <option value="Alabama">Alabama</option>
-                    <option value="Alaska">Alaska</option>
-                    <option value="Arizona">Arizona</option>
-                    <option value="Arkansas">Arkansas</option>
-                    <option value="California">California</option>
-                    <option value="Colorado">Colorado</option>
-                    <option value="Connecticut">Connecticut</option>
-                    <option value="Delaware">Delaware</option>
-                    <option value="Florida">Florida</option>
-                    <option value="Georgia">Georgia</option>
-                    <option value="Hawaii">Hawaii</option>
-                    <option value="Idaho">Idaho</option>
-                    <option value="Illinois">Illinois</option>
-                    <option value="Indiana">Indiana</option>
-                    <option value="Iowa">Iowa</option>
-                    <option value="Kansas">Kansas</option>
-                    <option value="Kentucky">Kentucky</option>
-                    <option value="Louisiana">Louisiana</option>
-                    <option value="Maine">Maine</option>
-                    <option value="Maryland">Maryland</option>
-                    <option value="Massachusetts">Massachusetts</option>
-                    <option value="Michigan">Michigan</option>
-                    <option value="Minnesota">Minnesota</option>
-                    <option value="Mississippi">Mississippi</option>
-                    <option value="Missouri">Missouri</option>
-                    <option value="Montana">Montana</option>
-                    <option value="Nebraska">Nebraska</option>
-                    <option value="Nevada">Nevada</option>
-                    <option value="New Hampshire">New Hampshire</option>
-                    <option value="New Jersey">New Jersey</option>
-                    <option value="New Mexico">New Mexico</option>
-                    <option value="New York">New York</option>
-                    <option value="North Carolina">North Carolina</option>
-                    <option value="North Dakota">North Dakota</option>
-                    <option value="Ohio">Ohio</option>
-                    <option value="Oklahoma">Oklahoma</option>
-                    <option value="Oregon">Oregon</option>
-                    <option value="Pennsylvania">Pennsylvania</option>
-                    <option value="Rhode Island">Rhode Island</option>
-                    <option value="South Carolina">South Carolina</option>
-                    <option value="South Dakota">South Dakota</option>
-                    <option value="Tennessee">Tennessee</option>
-                    <option value="Texas">Texas</option>
-                    <option value="Utah">Utah</option>
-                    <option value="Vermont">Vermont</option>
-                    <option value="Virginia">Virginia</option>
-                    <option value="Washington">Washington</option>
-                    <option value="West Virginia">West Virginia</option>
-                    <option value="Wisconsin">Wisconsin</option>
-                    <option value="Wyoming">Wyoming</option>
-                </Input>
-                <Button color="primary" onClick={() => { onCheckboxBtnClick(1); filterTrailsByState(stateFilter) }} active={cSelected.includes(1)}>State</Button>
 
-                <Input type='select' value={difficultyFilter} onChange={e => setDifficultyFilter(e.target.value)}>
-                    <option value="easy">easy</option>
-                    <option value="moderate">moderate</option>
-                    <option value="moderately strenuous">moderately strenuous</option>
-                    <option value="strenuous">strenuous</option>
-                    <option value="very strenuous">very strenuous</option>
-                </Input>
-                <Button color="primary" onClick={() => { onCheckboxBtnClick(2); filterTrailsByDifficulty(difficultyFilter) }} active={cSelected.includes(2)}>Difficulty</Button>
-            </ButtonGroup>
-            <Button onClick={() => clearFilter()}>clear filters</Button>
+            <Form onSubmit={e => filteredSearch(e)}>
+                <FormGroup>
+                    <input type='checkbox' id='stateCheckbox' value={stateFiltering} onChange={() => setStateFiltering(!stateFiltering)}></input>
+                    <Label for='stateCheckbox'>State</Label>
+                    {
+                        !stateFiltering ? null
+                            : <Input type='select' value={stateFilter} onChange={e => setStateFilter(e.target.value)}>
+                                <option value="Alabama">Alabama</option>
+                                <option value="Alaska">Alaska</option>
+                                <option value="Arizona">Arizona</option>
+                                <option value="Arkansas">Arkansas</option>
+                                <option value="California">California</option>
+                                <option value="Colorado">Colorado</option>
+                                <option value="Connecticut">Connecticut</option>
+                                <option value="Delaware">Delaware</option>
+                                <option value="Florida">Florida</option>
+                                <option value="Georgia">Georgia</option>
+                                <option value="Hawaii">Hawaii</option>
+                                <option value="Idaho">Idaho</option>
+                                <option value="Illinois">Illinois</option>
+                                <option value="Indiana">Indiana</option>
+                                <option value="Iowa">Iowa</option>
+                                <option value="Kansas">Kansas</option>
+                                <option value="Kentucky">Kentucky</option>
+                                <option value="Louisiana">Louisiana</option>
+                                <option value="Maine">Maine</option>
+                                <option value="Maryland">Maryland</option>
+                                <option value="Massachusetts">Massachusetts</option>
+                                <option value="Michigan">Michigan</option>
+                                <option value="Minnesota">Minnesota</option>
+                                <option value="Mississippi">Mississippi</option>
+                                <option value="Missouri">Missouri</option>
+                                <option value="Montana">Montana</option>
+                                <option value="Nebraska">Nebraska</option>
+                                <option value="Nevada">Nevada</option>
+                                <option value="New Hampshire">New Hampshire</option>
+                                <option value="New Jersey">New Jersey</option>
+                                <option value="New Mexico">New Mexico</option>
+                                <option value="New York">New York</option>
+                                <option value="North Carolina">North Carolina</option>
+                                <option value="North Dakota">North Dakota</option>
+                                <option value="Ohio">Ohio</option>
+                                <option value="Oklahoma">Oklahoma</option>
+                                <option value="Oregon">Oregon</option>
+                                <option value="Pennsylvania">Pennsylvania</option>
+                                <option value="Rhode Island">Rhode Island</option>
+                                <option value="South Carolina">South Carolina</option>
+                                <option value="South Dakota">South Dakota</option>
+                                <option value="Tennessee">Tennessee</option>
+                                <option value="Texas">Texas</option>
+                                <option value="Utah">Utah</option>
+                                <option value="Vermont">Vermont</option>
+                                <option value="Virginia">Virginia</option>
+                                <option value="Washington">Washington</option>
+                                <option value="West Virginia">West Virginia</option>
+                                <option value="Wisconsin">Wisconsin</option>
+                                <option value="Wyoming">Wyoming</option>
+                            </Input>
+                    }
+                </FormGroup>
+                <FormGroup>
+                    <input type='checkbox' id='difficultyCheckbox' value={difficultyFiltering} onChange={() => setDifficultyFiltering(!difficultyFiltering)}></input>
+                    <Label for='difficultyCheckbox'>Difficulty</Label>
+                    {
+                        !difficultyFiltering ? null
+                            : <Input type='select' value={difficultyFilter} onChange={e => setDifficultyFilter(e.target.value)}>
+                                <option value="easy">easy</option>
+                                <option value="moderate">moderate</option>
+                                <option value="moderately strenuous">moderately strenuous</option>
+                                <option value="strenuous">strenuous</option>
+                                <option value="very strenuous">very strenuous</option>
+                            </Input>
+                    }
+                </FormGroup>
+                <Button type='submit'>Submit</Button>
+                <Button onClick={clearFilters}>Clear filters</Button>
+            </Form>
 
             <Table>
                 <thead>
